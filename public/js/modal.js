07d5d7e47ec06a5f49d2ba45695e5dc4817ed91f
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-03-04 16:11:00 -0800

    fixed display of modal, not need to fix info

--- public/js/modal.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('#JobModalCenter')
-const myInput = document.getElementById('#JobModalCenter')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
