c841eba56a3e6377dcd798bb9f3553849f53812a
Andy Abebaw <andyabebaw1@gmail.com>
2023-03-06 11:15:08 -0600

    add space image

---
+++ public/js/homepage.js
@@ -0,0 +1,18 @@
+function getPOTD (){
+    var POTDURL = "https://api.nasa.gov/planetary/apod?api_key=HtijubiAOgIAt2267b0GcmJgmCM3ZouFNwKUWv8J";
+    fetch(POTDURL).then(function (response) {
+        
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        setImageBackground(data)
+      })
+}
+getPOTD()
+
+function setImageBackground(result){
+    $( ".wrap").css("background-image", "url(" + result.hdurl + ")");
+}
+
+
